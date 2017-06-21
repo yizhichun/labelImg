16e336bc071f176a36d2d1cffc3ade0ece575ac3
yizhichun <349480168@qq.com>
2017-06-21 11:13:09 +0800

    move vertex not ok

--- testGetAngle.py
+++ testGetAngle.py
@@ -24,13 +24,36 @@ def getAngle(center, p1, p2):
     else:
     	return -angle
 
+def getSymmetricalPoint(theta, p1, p2):
+    center = (p1 + p2) / 2
+    a = math.tan(theta)
+    b = center.y() - a * center.x()
+    X3 = p1.x() - 2*a*(a*p1.x()-p1.y()+b) / (a*a+1)
+    Y3 = p1.y() + 2*(a*p1.x()-p1.y()+b) / (a*a+1)
+    X4 = p2.x() - 2*a*(a*p2.x()-p2.y()+b) / (a*a+1)
+    Y4 = p2.y() + 2*(a*p2.x()-p2.y()+b) / (a*a+1)
+
+    p3 = QPointF(X3, Y3)
+    p4 = QPointF(X4, Y4)
+    return p3, p4
 
 
 if __name__ == '__main__':
-    center = QPointF(0,0)
-    p1 = QPointF(1,1)
-    p2 = QPointF(1,0)
-    angle = getAngle(center, p1, p2)
-    print(angle)
+
+    theta = math.pi/4;
+    p1=QPointF(1,0)
+    p2 = QPointF(-1,0)
+    p3,p4 = getSymmetricalPoint(theta,p1,p2)
+    print(p1)
+    print(p3)
+    print(p2)
+    print(p4)
+
+    # center = QPointF(0,0)
+    # p1 = QPointF(1,1)
+    # p2 = QPointF(1,0)
+    # print((p1+p2)/2)
+    # angle = getAngle(center, p1, p2)
+    # print(angle)
 
 
